b3c560dfa22c64174af814e5b78c1d97f841ab78
kauesimao <kauesimao1234@gmail.com>
2023-05-26 17:50:00 -0300

    quase terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SupplierComponent } from './supplier/supplier.component';
+import { ClientComponent } from './client/client.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SupplierComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/client.ts
@@ -0,0 +1,9 @@
+export interface Client {
+  id: number;
+  name: string;
+  email: string;
+  rg: string;
+  telefone: number;
+  sexo : string;
+  estado : string;
+}
